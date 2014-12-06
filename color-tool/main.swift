832efa456bd50de698b4402c186e42c05b7bda13
Markus Gasser <markus.gasser@konoma.ch>
2014-12-06 18:05:43 +0100

    Initial Commit

---
+++ color-tool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  color-tool
+//
+//  Created by Markus Gasser on 06.12.14.
+//  Copyright (c) 2014 konoma GmbH. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
